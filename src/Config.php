e0588ecb37d671e424c0a289312762dc1d2b8981
laur sandu <Sapistudio@users.noreply.github.com>
2018-09-17 01:40:47 +0300

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -6,6 +6,11 @@ class Config extends Document{
     protected static $uniqueIdentifier = 'id';
     protected $configOptions;
     
+    /**
+     * Config::__construct()
+     * 
+     * @return
+     */
     public function __construct($config = [])
     {
         foreach ($config as $key => $value)
@@ -14,19 +19,39 @@ class Config extends Document{
             $this->setDir($this->getOption('dir'));
     }
     
+    /**
+     * Config::setName()
+     * 
+     * @return
+     */
     public function setName($name)
     {
         return parent::setName($name.'.config');
     }
     
+    /**
+     * Config::getOption()
+     * 
+     * @return
+     */
     public function getOption($name){
         return (isset($this->configOptions->{$name})) ? $this->configOptions->{$name} : false;    
     }
     
+    /**
+     * Config::getIdentifier()
+     * 
+     * @return
+     */
     public function getIdentifier(){
         return self::$uniqueIdentifier;
     }
     
+    /**
+     * Config::setIdentifier()
+     * 
+     * @return
+     */
     public function setIdentifier($uid){
         self::$uniqueIdentifier = $uid;
     }
